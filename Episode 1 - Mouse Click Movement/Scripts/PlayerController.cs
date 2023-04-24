ed81bea4b4024734131682b3ae9592896e04a029
ItsPogle <poglyrs@gmail.com>
2023-04-24 19:43:56 +1000

    Episode 1

---
+++ Episode 1 - Mouse Click Movement/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    const string IDLE = "Idle";
+    const string WALK = "Walk";
+
+    CustomActions input;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    [Header("Movement")]
+    [SerializeField] ParticleSystem clickEffect;
+    [SerializeField] LayerMask clickableLayers;
+
+    float lookRotationSpeed = 8f;
+    
+    void Awake() 
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        input = new CustomActions();
+        AssignInputs();
+    }
+
+    void AssignInputs()
+    {
+        input.Main.Move.performed += ctx => ClickToMove();
+    }
+
+    void ClickToMove()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers)) 
+        {
+            agent.destination = hit.point;
+            if(clickEffect != null)
+            { Instantiate(clickEffect, hit.point + new Vector3(0, 0.1f, 0), clickEffect.transform.rotation); }
+        }
+    }
+
+    void OnEnable() 
+    { input.Enable(); }
+
+    void OnDisable() 
+    { input.Disable();}
+
+    void Update() 
+    {
+        FaceTarget();
+        SetAnimations();
+    }
+
+    void FaceTarget()
+	{
+        Vector3 direction = (agent.destination - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+	}
+
+    void SetAnimations()
+    {
+        if(agent.velocity == Vector3.zero)
+        { animator.Play(IDLE); }
+        else
+        { animator.Play(WALK); }
+    }
+}
